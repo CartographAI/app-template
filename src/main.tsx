3585b45bd2e1ca0880bd81bca0343324a91ad2e8
Si-Yan Teo <dev@teosiyan.com>
2024-12-12 18:32:25 +0800

    feat: Set up tanstack router provider with root and index routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,25 @@
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { routeTree } from "./routeTree.gen";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find root element");
 
 createRoot(rootElement).render(
   <StrictMode>
-    <App />
+    <MantineProvider>
+      <RouterProvider router={router} />
+    </MantineProvider>
   </StrictMode>,
 );
